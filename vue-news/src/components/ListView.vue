38314232502e65dbbc04c62adc166cdace15491a
jmleeh4 <jmleeh4@naver.com>
2022-09-01 13:57:44 +0900

    20220901 commit

---
+++ vue-news/src/components/ListView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <ul class="news-list">
+      <li v-for="item in fetched_news" class="post">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a :href="item.url">{{ item.title }}</a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+            by {{ item.time_ago }}
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  name: "ListView",
+  computed: {
+    ...mapGetters(['fetched_news'])
+  },
+  created() {
+    this.$store.dispatch('FETCH_NEWS');
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+
+.news-title {
+  margin: 0;
+}
+
+.link-text {
+  color: #828282;
+}
+</style>
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,63 +1,15 @@
 <template>
-  <div>
-    <ul class="news-list">
-      <li v-for="item in fetched_news" class="post">
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <div>
-          <p class="news-title">
-            <a :href="item.url">{{ item.title }}</a>
-          </p>
-          <small class="link-text">
-            by <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link> by {{ item.time_ago }}
-          </small>
-        </div>
-      </li>
-    </ul>
-  </div>
+  <list-view></list-view>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import ListView from "../components/ListView";
 
 export default {
-  name: "NewsView",
-  computed: {
-    ...mapGetters(['fetched_news'])
-  },
-  created() {
-    this.$store.dispatch('FETCH_NEWS');
-  },
-  methods: {}
+  components: {
+    ListView
+  }
 }
 </script>
 
-<style scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
 
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #828282;
-}
-</style>
